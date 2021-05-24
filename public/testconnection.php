bc7ddfb1b79778a21f195572c0d628dad585e3fb
chris2274 <llyy8106@163.com>
2021-05-24 14:20:37 +0800

    aks

--- public/testconnection.php
+++ public/testconnection.php
@@ -1,5 +1,10 @@
 <?php
-$link = mysqli_connect('10.0.1.162', 'chris', 'P@ssw0rd!');
+
+$connectstr_dbhost = getenv('DATABASE_HOST');
+$connectstr_dbusername = getenv('DATABASE_USERNAME');
+$connectstr_dbpassword = getenv('DATABASE_PASSWORD');
+
+$link = mysqli_connect($connectstr_dbhost, $connectstr_dbusernam, $connectstr_dbpassword);
 if (!$link) {
 die('Could not connect: ' . mysqli_error());
 }
